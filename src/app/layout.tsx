a2aef5e90db880bf1f987fb63c1ed70a09825013
DukeZhu95 <lz320@students.waikato.ac.nz>
2024-12-31 18:50:00 +1300

    v3.4 Fixed errors for deploying

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
 'use client';
 
-import { ClerkProvider, useAuth } from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { ConvexReactClient } from 'convex/react';
-import './globals.css';
+import { useAuth } from '@clerk/nextjs';
 import React from 'react';
-
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-
-function ConvexClientProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      {children}
-    </ConvexProviderWithClerk>
-  );
-}
 
 export default function RootLayout({
   children,
@@ -25,9 +16,19 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ClerkProvider>
-          <ConvexClientProvider>{children}</ConvexClientProvider>
+          <ClientConvexProvider>{children}</ClientConvexProvider>
         </ClerkProvider>
       </body>
     </html>
+  );
+}
+
+function ClientConvexProvider({ children }: { children: React.ReactNode }) {
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
   );
 }
